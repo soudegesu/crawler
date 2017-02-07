d8966e99df99c9f6f4ad11ec4d8cabd778ab3ae4
soudegesu <soudegesu@hotmail.co.jp>
2017-02-08 01:08:02 +0900

    consider redirect relation (n:1)

--- init.d/init.sql
+++ init.d/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS crawl.page (
   id int AUTO_INCREMENT,
-  url varchar(255) NOT NULL UNIQUE,
+  url varchar(255),
   previous_url varchar(255) NOT NULL UNIQUE,
   status int NOT NULL,
   parent_id int NOT NULL,
